ede0bf278c9afe637349ee9eb39e8da1373a3ba8
egd-ecrespi <etienne.crespi@eggers-digital.com>
2020-02-14 17:16:20 +0100

    Implement FOSUserBundle with config files

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
